c13ae4b076c76a0ed1468e8af7467d23ce348c8b
osman.ozden <osmanozden@Osmans-MacBook-Pro.local>
2024-07-19 18:07:46 +0300

    first com

---
+++ src/main/java/com/joinzad/interviewcase/InterviewCaseApplication.java
@@ -0,0 +1,13 @@
+package com.joinzad.interviewcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joinzad/interviewcase/InterviewCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joinzad.interviewcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
