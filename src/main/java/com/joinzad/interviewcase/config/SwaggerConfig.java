35ec8afebf052feadd7c01f9150013bcb39b3577
osman.ozden <osmanozden@Osmans-MacBook-Pro.local>
2024-07-19 19:01:49 +0300

    Redis and Swagger Confiration added.

---
+++ src/main/java/com/joinzad/interviewcase/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.joinzad.interviewcase.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Account API")
+                        .version("1.0")
+                        .description("API for managing user accounts"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("accounts")
+                .pathsToMatch("/accounts/**")
+                .build();
+    }
+}
