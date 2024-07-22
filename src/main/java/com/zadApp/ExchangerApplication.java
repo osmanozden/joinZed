dad75421be1b1688dc36073ce3cc2a4873430f98
osman.ozden <osmanozden@Osmans-MacBook-Pro.local>
2024-07-22 09:18:09 +0300

    code cleaned

--- src/main/java/com/zadApp/ExchangerApplication.java
+++ src/main/java/com/zadApp/ExchangerApplication.java
@@ -1,11 +1,8 @@
 package com.zadApp;
 
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class ExchangerApplication {
