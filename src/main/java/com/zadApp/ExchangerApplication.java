02506477c3002fa2aade3cd7b59ceb9364bebb77
osman.ozden <osmanozden@Osmans-MacBook-Pro.local>
2024-07-22 05:26:30 +0300

    ref

--- src/main/java/com/zadApp/ExchangerApplication.java
+++ src/main/java/com/zadApp/ExchangerApplication.java
@@ -1,15 +1,19 @@
 package com.zadApp;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExchangerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExchangerApplication.class, args);
-
     }
+
+
 
 }
--- src/main/java/com/zadApp/config/SwaggerConfig.java
+++ src/main/java/com/zadApp/config/SwaggerConfig.java
@@ -27,4 +27,5 @@ public class SwaggerConfig {
                 .pathsToMatch("/accounts/**")
                 .build();
     }
+
 }
